b4c52b1058293b66b3d98832f0ba99f9769dacf5
leakyroot <rootleaky@gmail.com>
2025-01-21 19:08:14 +0100

    b: added FPS control (set to 60)

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
 	pygame.init()
+	clock = pygame.time.Clock()
+	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	while True:
 		for event in pygame.event.get():
@@ -12,6 +14,7 @@ def main():
 				return
 		
 		screen.fill(000000)
+		dt = clock.tick(60) / 1000
 
 
 
@@ -20,6 +23,7 @@ def main():
 	print(f"Screen height: {SCREEN_HEIGHT}")
 
 	pygame.display.flip()
+	
 
 if __name__ == "__main__":
 	main()
